46a5824496e661e5534a2ea80b7ed6d4076f255d
cynthiajteo <cynthiajteo@gmail.com>
2021-02-16 18:56:05 +0800

    linked to mongo atlas

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const productController = require('./controllers/products');
 const methodOverride = require('method-override');
-// require('dotenv').config();
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/basiccrud', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-// mongoose.connect(process.env.mongoURL, {
+// mongoose.connect('mongodb://localhost:27017/basiccrud', {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 // });
+
+mongoose.connect(process.env.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 mongoose.connection.once('open', () => {
     console.log('connected to mongo');
